42121826a15bb3b10c826632bc15ffc86e99961d
mafzaldev <afzalashraf814@gmail.com>
2022-09-13 19:58:26 +0500

    added media query

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -4,15 +4,23 @@ import TodoItem from "./TodoItem";
 export default function TodoList({ todoList, onDelete, onEdit, onCheck }) {
   return (
     <ul className="list">
-      {todoList.map((todo, index) => (
-        <TodoItem
-          key={index}
-          todo={todo}
-          onDelete={onDelete}
-          onEdit={onEdit}
-          onCheck={onCheck}
-        />
-      ))}
+      {todoList.length > 0 ? (
+        todoList.map((todo, index) => (
+          <TodoItem
+            key={index}
+            todo={todo}
+            onDelete={onDelete}
+            onEdit={onEdit}
+            onCheck={onCheck}
+          />
+        ))
+      ) : (
+        <span>
+          Nothing to find here,
+          <br />
+          Use the above field to add a todo.
+        </span>
+      )}
     </ul>
   );
 }
